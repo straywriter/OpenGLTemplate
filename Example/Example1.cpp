8bd36161f28a6bda603a61b4eb888a69f9b2dcf4
TA-MEI <2745982906@qq.com>
2020-12-27 19:10:41 +0800

    2020-12-27 update

--- Example/Example1.cpp
+++ Example/Example1.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file Example1.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2020-12-02
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
